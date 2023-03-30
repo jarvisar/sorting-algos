51b71db9063b171585b8f5c4bcf6b169975724d7
jarvisar <jarvisar@mail.uc.edu>
2023-03-29 20:55:16 -0400

    Fixed bubble sort

--- src/app/sorting-visualizer/sorting-visualizer.component.ts
+++ src/app/sorting-visualizer/sorting-visualizer.component.ts
@@ -18,41 +18,47 @@ export class SortingVisualizerComponent implements OnInit {
   stopSorting = false;
   async sort() {
     let n = this.barHeights.length;
-    let swapped = true; // Set to true to enter the loop
-    const delay = 10; // delay in milliseconds between iterations
-  
+    let swapped = true;
+    const delay = 500;
     const sleep = (ms: number) => {
       return new Promise(resolve => setTimeout(resolve, ms));
     };
   
+    // Set all unsorted bars to red
+    const setUnsortedToRed = () => {
+      for (let i = 0; i < n; i++) {
+        this.setBarColor(i, 'red');
+      }
+    };
+  
     while (swapped) {
       swapped = false;
+      setUnsortedToRed(); // Set all unsorted bars to red at the beginning of each loop iteration
       for (let i = 0; i < n - 1; i++) {
-        // Check if sorting should be stopped
         if (this.stopSorting) {
           return;
         }
-        // Set the color of the bars being compared to red
-        this.setBarColor(i, 'red');
-        this.setBarColor(i + 1, 'red');
-  
+        // Set the color of the bars being compared to blue
+        this.setBarColor(i + 1, 'blue');
         await sleep(delay);
-  
         // Swap the bars if they're out of order
         if (this.barHeights[i] > this.barHeights[i + 1]) {
           let temp = this.barHeights[i];
           this.barHeights[i] = this.barHeights[i + 1];
           this.barHeights[i + 1] = temp;
           swapped = true;
+          this.setBarColor(i, 'yellow');
+          this.setBarColor(i + 1, 'red');
+        } else {
+          // Set the color of the bars being compared back to red if they are not swapped
+          this.setBarColor(i, 'red');
+          this.setBarColor(i + 1, 'red');
         }
-  
-        // Set the color of the bars being compared back to their default color
-        this.setBarColor(i, '');
-        this.setBarColor(i + 1, '');
       }
+      // Set the bar in its final position to green
+      this.setBarColor(n - 1, 'green');
       n--;
     }
-  
     // Mark all bars as sorted
     for (let i = 0; i < this.barHeights.length; i++) {
       this.setBarColor(i, 'green');
@@ -66,11 +72,10 @@ export class SortingVisualizerComponent implements OnInit {
     bar.style.backgroundColor = color;
   }
 
-  stop() {
-    // Set all bar colors to green to indicate sorting is stopped
-    for (let i = 0; i < this.barHeights.length; i++) {
-      this.setBarColor(i, 'green');
-    }
+  stop(){
+    //toggle
+    this.stopSorting = !this.stopSorting;
+
   }
   
   
