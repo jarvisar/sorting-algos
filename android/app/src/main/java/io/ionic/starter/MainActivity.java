e3e502353f724dc56f471fafc5ef3ed83ab88a35
jarvisar <jarvisar@mail.uc.edu>
2023-04-02 17:34:57 -0400

    Finished android app

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,5 +1,20 @@
 package io.ionic.starter;
 
+import android.os.Bundle;
+import android.view.WindowManager;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+ 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Hide notification bar and navigation buttons
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                             WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // Rest of your code
+    }
+}
