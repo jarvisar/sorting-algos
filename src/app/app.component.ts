33dadfc1a6965fe9cf8147c248d86772d395a40b
jarvisar <jarvisar@mail.uc.edu>
2023-04-04 21:43:47 -0400

    Added volume

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,27 @@ import { Component } from '@angular/core';
 import { SortService } from './sort.service';
 import { ViewChild } from '@angular/core';
 import { InputBarComponent } from './input-bar/input-bar.component';
+import { trigger, transition, style, animate, state } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('modalState', [
+      state('true', style({
+        opacity: '1'
+      })),
+      state('false', style({
+        opacity: '0'
+      })),
+      transition('* => *', animate('200ms ease'))
+    ])
+  ]
 })
 export class AppComponent {
   title = 'sorting-algos';
+  isShown = false;
 
   @ViewChild(InputBarComponent, { static: false }) inputbar!: InputBarComponent;
   constructor( public sortService: SortService) {
@@ -30,6 +43,14 @@ export class AppComponent {
   
   openHelp(){
     window.open('https://www.geeksforgeeks.org/sorting-algorithms/');
+  }
+
+  openVolume(){
+    this.isShown = true;
+  }
+
+  closeVolume(){
+    this.isShown = false;
   }
   
 }
